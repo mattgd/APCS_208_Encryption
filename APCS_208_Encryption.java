d5e972e30f3aa9854e942ffb9ee2bce55d0ea196
Matt <mattgd@users.noreply.github.com>
2015-11-08 11:23:31 -0500

    Update APCS_208_Encryption.java

--- APCS_208_Encryption.java
+++ APCS_208_Encryption.java
@@ -1,7 +1,7 @@
 /**
  * @(#)APCS_208_Encryption.java
  *
- * @author mattgd
+ * @author Matt Dzwonczyk
  * @version 1.00 2015/10/30
  */
 
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 class Cypher {
 	
-	private int[][] key = { {1, 2}, {2, 8} };
+	private double[][] key = { {1, 2}, {2, 8} };
 	private String message = "";
 	
 	Cypher() {		
@@ -25,7 +25,7 @@ class Cypher {
     		e.printStackTrace();
     	}
     	
-    	encrypt(message);
+    	message = decrypt(encrypt(message));
 	}
 	
 	public String encrypt(String data) {
@@ -56,29 +56,10 @@ class Cypher {
 			if (count == 2) count = 0;
 		}
 		
-		// Create an array of proper size for the encrypted numbers
-		double[][] encryptedArray = new double[dataArray.length][2];
 		
-		count = 0;
-		for (int i = 0; i < dataArray.length; i++) {
-			for (int j = 0; j < 2; j++) {
-				// This prints out all of the information for testing purposes
-				//System.out.printf("{ TOPROW: %d, I: %d, J: %d, COUNT: %d, ENCRYPTED ROW INDEX: %d, DATA ARRAY ROW INDEX: %d, DATA ARRAY: %.2f, KEY ROW INDEX: %d, DATAARRAY NEW: %.2f }%n", topRow, i, j, count, i + topRow % 2, i, dataArray[i * 2][j], key[i % 2][j], dataArray[i * 2][j] * key[i % 2][j]);
-
-				if (isEven(j)) {
-					encryptedArray[i + j + topRow % 2][j] = dataArray[i][j] * key[i % 2][j];
-				} else {
-					encryptedArray[i + topRow % 2][j] = dataArray[i][j] * key[i % 2][j];
-				}
-			}
-			
-			// Increment the topRow
-			count++;
-			if (count == 2) {
-				count = 0;
-				topRow += 2;
-			}
-		}
+		
+		// Create an array of proper size for the encrypted numbers
+		double[][] encryptedArray = multiply(dataArray, (double[][]) key);
 		
 		// Turn the array into a "coded" String
 		data = "";
@@ -97,8 +78,79 @@ class Cypher {
 	}
 	
 	public String decrypt(String data) {
+		String validChars = "0123456789- ";
+		String selectedChar;
+		String nextCode = "";
+		int topRow = 0; // Will be used later to keep track of which section the matrix is being multiplied by
+
+		int count = 0;
+		for (int i = 0; i < data.length(); i++) {
+			if (data.charAt(i) == '-') {
+				count++;
+			}
+		}
 		
+		double[][] dataArray = new double[count][2];
+		count = 0; // Now used to keep track of Array index
+		
+		for (int i = 0; i < data.length(); i++) {
+			selectedChar = "" + data.charAt(i);
+			
+			// Remove unwated characters in front of number data
+			if (validChars.contains(selectedChar)) {
+				nextCode += selectedChar;
+				data.replaceFirst(selectedChar, "");
+				
+				if (!validChars.contains("" + data.charAt(i + 1))) {
+					dataArray[count][0] = Double.parseDouble(nextCode.substring(0, nextCode.indexOf("-")));
+					dataArray[count][1] = Double.parseDouble(nextCode.substring(nextCode.indexOf("-") + 1));
+					nextCode = "";
+					count++;
+				}
+			} else {
+				data.replaceFirst(selectedChar, "");
+			}
+		}
+		
+		// Decrypt
+		double[][] inverse = { {2, -.5}, {-.5, .25} };
+		double[][] decryptedArray = multiply(dataArray, inverse);
+		
+		// Convert to letters
+		char currentChar;
+		data = "";
+		
+		for (int i = 0; i < decryptedArray.length; i++) {
+			for (int j = 0; j < decryptedArray[0].length; j++) {
+				if (decryptedArray[i][j] == 27) {
+					currentChar = ' ';
+				} else {
+					currentChar = (char) (decryptedArray[i][j] + 64);
+				}
+				
+				data += currentChar;
+			}
+		}
+		
+		data = data.substring(0, data.length() - 1); // Remove end-of-line character
+		
+		return data;
 	}
+	
+	// return C = A * B
+    public static double[][] multiply(double[][] A, double[][] B) {
+        int mA = A.length;
+        int nA = A[0].length;
+        int mB = B.length;
+        int nB = B[0].length;
+        if (nA != mB) throw new RuntimeException("Illegal matrix dimensions.");
+        double[][] C = new double[mA][nB];
+        for (int i = 0; i < mA; i++)
+            for (int j = 0; j < nB; j++)
+                for (int k = 0; k < nA; k++)
+                    C[i][j] += A[i][k] * B[k][j];
+        return C;
+    }
     
     public void printArray(double[][] dataArray) {
     	String test = "[";
@@ -126,12 +178,17 @@ class Cypher {
     	if (i % 2 == 0) return true;
     	return false;
     }
+    
+    public String getMessage() {
+    	return message;
+    }
 }
 
 public class APCS_208_Encryption {
 	
     public static void main(String[] args) {
         Cypher cypher = new Cypher();
+        System.out.println(cypher.getMessage());
     }
     
 }
